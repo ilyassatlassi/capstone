cafda62e8b94aa2c9114c44145bb40db676fe95f
ilyass atlassi <atlassiilyass@gmail.com>
2022-12-02 21:19:43 +0100

    Update the readme file

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# capstone
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Designing a Responsive page Using HTML&CSS
+
+
+> Description the project.
+
+
+## Built With
+
+- html&css
+- no Frameworks
+- Flexbox
+- linter
+
+## Link to online version of my capstone
+
+https://github.com/ilyassatlassi/capstone
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+All you need VScode compiler or a compiler of your choice.
+
+### Setup
+
+You will need to clone the url from the source repository and run the following command 
+git clone https://github.com/ilyassatlassi/capstone.git
+
+
+## Outline
+
+- [@demo](https://www.loom.com/share/c9d1cdea8e4141ac81eecb4c591c5987)
+## Author
+
+👤 ilyass atlassi
+
+- GitHub: [@githubhandle](https://github.com/ilyassatlass)
+- Twitter: [@twitterhandle](https://twitter.com/ilyass_atlassi)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/ilyassatlassi/)
+
+
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](https://github.com/ilyassatlassi/portfolio_setup_mobile/blob/main/LICENCE.md) licensed.
+
+_NOTE: we recommend using the [MIT license](https://choosealicense.com/licenses/mit/) - you can set it up quickly by [using templates available on GitHub](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository). You can also use [any other license](https://choosealicense.com/licenses/) if you wish._
--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-const deleted = document.querySelector(".delete");
-const nav = document.querySelector(".second-nav")
-const navcont = document.querySelector(".second-nav-content")
+const deleted = document.querySelector('.delete');
+const nav = document.querySelector('.second-nav');
+const navcont = document.querySelector('.second-nav-content');
 const hamburger = document.querySelector('.toolbar');
-const feature = document.querySelector('.featured')
+const feature = document.querySelector('.featured');
 
-//menu button
+// menu button
 const activeToggle = () => {
-    navcont.classList.toggle('active');
-    nav.style.display = 'flex';
+  navcont.classList.toggle('active');
+  nav.style.display = 'flex';
 };
 
 const deleteToggle = () => {
-    navcont.classList.toggle('active');
-    nav.style.display = 'none';
+  navcont.classList.toggle('active');
+  nav.style.display = 'none';
 };
-  
+
 hamburger.addEventListener('click', activeToggle);
 deleted.addEventListener('click', deleteToggle);
 
 // Add the feature section
 const featured = [
-    {
-      title: 'Refereeing',
-      caption: 'Collina: We’ve asked referees to calculate stoppage time more accurately',
-      image: './images/first.webp',
-    },
-    {
-      title: 'Football Unites the world',
-      caption: 'Milla: Some might say football is more important than politics ',
-      image: './images/second.webp',
-    },
-    {
-      title: 'FIFA 22',
-      caption: 'FIFA 22 is a football simulation video game.',
-      image: './images/fifa-22.jpg',
-    },
-    {
-      title: 'Organization',
-      caption: 'Enrique Macaya Márquez makes it 17 FIFA World Cups in a row',
-      image: './images/third.webp',
-    },
-    {
-      title: 'Football Unites the world',
-      caption: ' Hristo Stoichkov: The World Cup Cup is a family party ',
-      image: './images/four.webp',
-    },
-    {
-      title: 'Footaball Unites the world',
-      caption: 'Ian Wright: Football naturally brings people together ',
-      image: './images/five.webp',
-    },
-  ];
+  {
+    title: 'Refereeing',
+    caption: 'Collina: We’ve asked referees to calculate stoppage time more accurately',
+    image: './images/first.webp',
+  },
+  {
+    title: 'Football Unites the world',
+    caption: 'Milla: Some might say football is more important than politics ',
+    image: './images/second.webp',
+  },
+  {
+    title: 'FIFA 22',
+    caption: 'FIFA 22 is a football simulation video game.',
+    image: './images/fifa-22.jpg',
+  },
+  {
+    title: 'Organization',
+    caption: 'Enrique Macaya Márquez makes it 17 FIFA World Cups in a row',
+    image: './images/third.webp',
+  },
+  {
+    title: 'Football Unites the world',
+    caption: ' Hristo Stoichkov: The World Cup Cup is a family party ',
+    image: './images/four.webp',
+  },
+  {
+    title: 'Footaball Unites the world',
+    caption: 'Ian Wright: Football naturally brings people together ',
+    image: './images/five.webp',
+  },
+];
 
 const featuresec = document.createElement('h1');
 featuresec.innerText = 'Featured';
@@ -68,52 +68,51 @@ rightmaincard.classList.add('rigmaincard');
 const leftmaincard = document.createElement('div');
 leftmaincard.classList.add('leftmaincard');
 
+for (let i = 0; i < featured.length && i < 3; i += 1) {
+  const maindivcard = document.createElement('div');
+  maindivcard.classList.add('maincard');
+  const imagediv = document.createElement('div');
+  imagediv.classList.add('imagediv');
+  const cardimg = document.createElement('img');
+  cardimg.src = featured[i].image;
+  imagediv.append(cardimg);
+  const maintextdiv = document.createElement('div');
+  maintextdiv.classList.add('maintextdiv');
+  const maintextdivh1 = document.createElement('h2');
+  maintextdivh1.innerText = featured[i].title;
+  const maintextdivh3 = document.createElement('h3');
+  maintextdivh3.innerText = featured[i].caption;
+  maintextdiv.append(
+    maintextdivh1,
+    maintextdivh3,
+  );
+  maindivcard.append(imagediv, maintextdiv);
 
-  for (let i = 0; i < featured.length && i < 3; i ++) {
-    const maindivcard = document.createElement('div');
-    maindivcard.classList.add('maincard');
-    const imagediv = document.createElement('div');
-    imagediv.classList.add('imagediv');
-    const cardimg = document.createElement('img');
-    cardimg.src = featured[i].image;
-    imagediv.append(cardimg);
-    const maintextdiv = document.createElement('div');
-    maintextdiv.classList.add('maintextdiv');
-    const maintextdivh1 = document.createElement('h2');
-    maintextdivh1.innerText = featured[i].title;
-    const maintextdivh3 = document.createElement('h3');
-    maintextdivh3.innerText = featured[i].caption;
-    maintextdiv.append(
-      maintextdivh1,
-      maintextdivh3,
-    );
-    maindivcard.append(imagediv, maintextdiv);
-  
-    rightmaincard.append(maindivcard);
-  }
+  rightmaincard.append(maindivcard);
+}
 
-  for (let i = 3; i < featured.length && i < 6; i ++) {
-    const maindivcard = document.createElement('div');
-    maindivcard.classList.add('maincard');
-    const imagediv = document.createElement('div');
-    imagediv.classList.add('imagediv');
-    const cardimg = document.createElement('img');
-    cardimg.src = featured[i].image;
-    imagediv.append(cardimg);
-    const maintextdiv = document.createElement('div');
-    maintextdiv.classList.add('maintextdiv');
-    const maintextdivh1 = document.createElement('h2');
-    maintextdivh1.innerText = featured[i].title;
-    const maintextdivh3 = document.createElement('h3');
-    maintextdivh3.innerText = featured[i].caption;
-    maintextdiv.append(
-      maintextdivh1,
-      maintextdivh3,
-    );
-    maindivcard.append(imagediv, maintextdiv);
-  
-    leftmaincard.append(maindivcard);
-  }
-  
-  featurecard.append(rightmaincard, leftmaincard);
-  feature.append(featurecard)
+for (let i = 3; i < featured.length && i < 6; i += 1) {
+  const maindivcard = document.createElement('div');
+  maindivcard.classList.add('maincard');
+  const imagediv = document.createElement('div');
+  imagediv.classList.add('imagediv');
+  const cardimg = document.createElement('img');
+  cardimg.src = featured[i].image;
+  imagediv.append(cardimg);
+  const maintextdiv = document.createElement('div');
+  maintextdiv.classList.add('maintextdiv');
+  const maintextdivh1 = document.createElement('h2');
+  maintextdivh1.innerText = featured[i].title;
+  const maintextdivh3 = document.createElement('h3');
+  maintextdivh3.innerText = featured[i].caption;
+  maintextdiv.append(
+    maintextdivh1,
+    maintextdivh3,
+  );
+  maindivcard.append(imagediv, maintextdiv);
+
+  leftmaincard.append(maindivcard);
+}
+
+featurecard.append(rightmaincard, leftmaincard);
+feature.append(featurecard);
